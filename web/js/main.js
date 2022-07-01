3355fcd9791c3daa925dae01697844ac3679b364
johannespanzer <johannes-panzer@gmx.de>
2022-07-01 10:05:29 +0200

    wip session settings

--- web/js/main.js
+++ web/js/main.js
@@ -1,13 +1,44 @@
 import Timer from './components/Timer.js';
-import "./../css/main.less"
+import './../css/main.less';
 
-export class Main {
+export class Main
+{
 
-    initMain() {
-        console.log('main')
+    initMain()
+    {
     }
 
-    initTimer() {
+    initTimer()
+    {
         new Timer();
     }
+}
+
+const $navbar = document.querySelector('.navbar');
+if ($navbar) {
+    window.onscroll = function () {
+        //addPseudoElement($navbar);
+        addFixed();
+    };
+}
+
+function addFixed()
+{
+    const navbarOffset = $navbar.offsetTop + $navbar.offsetHeight;
+    if (window.scrollY >= navbarOffset) {
+        $navbar.classList.add('fixed-top');
+    } else {
+        $navbar.classList.remove('fixed-top');
+    }
+}
+
+function addPseudoElement(element)
+{
+    if (document.querySelector('.pseudoElement')) {
+        document.querySelector('.pseudoElement').remove();
+    }
+    const $pseudoElement     = document.createElement('div');
+    $pseudoElement.innerHTML = `<div class="pseudoElement" style="height: ${element.offsetHeight} "><p style="visibility: hidden">.</p></div>`;
+    element.after($pseudoElement.firstChild);
+
 }
